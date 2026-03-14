ea38ab06d49ca4c4ea0026928f88bda0ae1bf936
Aryan Kalra <aryan.smart832@gmail.com>
2026-03-14 17:59:04 +0530

    add export-donload route

---
+++ app/api/export-download/[id]/route.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import fs from "fs";
+import { NextResponse } from "next/server";
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const { id } = await params;
+
+  try {
+    const filePath = path.join(process.cwd(), "app/export", `export-${id}.csv`);
+
+    if (!fs.existsSync(filePath)) {
+      return NextResponse.json({ message: "File not ready" }, { status: 404 });
+    }
+
+    const fileBuffer = fs.readFileSync(filePath);
+
+    return new NextResponse(fileBuffer, {
+      status: 200,
+      headers: {
+        "Content-Type": "text/csv",
+        "Content-Disposition": `attachment; filename="users-${id}.csv"`,
+      },
+    });
+  } catch (error) {
+    console.error("Download Failed", error);
+    return Response.json({ error: "Failed to download file" }, { status: 500 });
+  }
+}
