6c85560742d2525c0b8a31a5c6e45bbc61c4f1f1
Aryan Kalra <aryan.smart832@gmail.com>
2026-03-14 22:39:22 +0530

    add query route

---
+++ app/api/users/query/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    console.log("searchParams", searchParams);
+    const cursor = searchParams.get("cursor");
+    const pageSize = searchParams.get("size");
+    const page = searchParams.get("page");
+    const country = searchParams.get("country");
+
+    const query = `
+  SELECT
+    id,
+    first_name,
+    last_name,
+    email,
+    country,created_at
+  FROM "user"
+  WHERE country = $1
+  ORDER BY id ASC
+  OFFSET ($2 - 1) * $3
+  LIMIT $3
+`;
+
+    const data = await db.$queryRawUnsafe(query, country, page, pageSize);
+
+    return NextResponse.json({
+      data,
+    });
+  } catch (error) {
+    console.error("Failed to list users data");
+    return NextResponse.json({
+      message: "Failed to filter users data",
+    });
+  }
+}
